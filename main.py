b78be00cfeab1e27b8578a05a6d1ceed49281e69
Philipp Fröhlich <philipp-froehlich@hotmail.com>
2017-06-05 23:52:10 +0200

    Implement simple departure feature

--- main.py
+++ main.py
@@ -5,18 +5,94 @@ import time
 import os
 import sys
 import subprocess
+import http.client
+import json2html
+import datetime
 
+from json2html import *
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.delegate import per_chat_id_in, create_open, pave_event_space, include_callback_query_chat_id
 from pprint import pprint
-from apscheduler.schedulers.background import BackgroundScheduler
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
-from apscheduler.job import Job
 
 APPNAME = "mvv_bot"
 APPVERSION = "1.0.0"
 
+
+def get_mvg_auth_key() :
+    global mvg_auth_key
+    return mvg_auth_key
+
+def get_stations_close_to(location) :
+    global verbose
+    latitude = location.get('latitude')
+    longitude = location.get('longitude')
+
+    conn = http.client.HTTPSConnection("www.mvg.de")
+
+    headers = {
+        'x-mvg-authorization-key': get_mvg_auth_key(),
+        'cache-control': "no-cache",
+        }
+
+    conn.request("GET", '/fahrinfo/api/location/nearby?latitude={}&longitude={}'.format(latitude,longitude), headers=headers)
+
+    res = conn.getresponse()
+    result = json.loads(res.read().decode("utf-8"))
+
+    # order by distance desc
+    try : 
+        locations = result.get('locations')
+        if len(locations) > 3:
+            return locations[0:3]
+        else :
+            return locations
+    except ValueError as e:
+        return ""
+
+def getNameFromLocation(x) : 
+    try:
+        return x.get("name")
+    except ValueError:
+        return ""
+
+def projectDeparture(departure):
+    departure.pop("departureId", None)
+    departure.pop("lineBackgroundColor", None)
+    if not departure.get("sev"):
+        departure.pop("sev", None)
+    
+    return departure
+
+
+def get_Departures_From_Location(location_id) :
+    global verbose
+     
+    conn = http.client.HTTPSConnection("www.mvg.de")
+
+    headers = {
+        'x-mvg-authorization-key': get_mvg_auth_key(),
+        'cache-control': "no-cache",
+        }
+
+    conn.request("GET", '/fahrinfo/api/departure/{}'.format(location_id), headers=headers)
+
+    res = conn.getresponse()
+    result = json.loads(res.read().decode("utf-8"))
+    
+    if verbose:
+        print(result)
+
+    return list(map(projectDeparture, result.get("departures")))
+
+
+  
+def parseDepartures(departures) :
+    result = "**Abfahrt**  **Linie**  **Ziel**\n" 
+    for departure in departures :
+        depTime = time.strftime("%m. %b. %H:%M:%S" , time.localtime(departure.get("departureTime")/1e3))
+        result += '{}  |  {}    {} \n'.format(depTime, departure.get("product").capitalize()+departure.get("label") ,departure.get("destination"))
+
+    return result
 
 class easydict(dict):
     def __missing__(self, key):
@@ -59,7 +135,7 @@ class ChatUser(telepot.helper.ChatHandler):
         self.sender.sendMessage('Wähle eine Aktion:', reply_markup=keyboard)
 
     def on_chat_message(self, msg):
-        global scheduler, settings, alerting_on
+        global settings, alerting_on, home_location
         content_type, chat_type, chat_id = telepot.glance(msg)
         if content_type == 'text':
             if self.verbose:
@@ -74,8 +150,7 @@ class ChatUser(telepot.helper.ChatHandler):
                                         parse_mode='Markdown')
                 self.send_main_menu()
             elif msg_text.startswith('/dep'):
-                print("Calculating departures")
-                self.sender.sendMessage('Calculate departures from location')
+                self.getDepartures(home_location)
             elif msg_text.startswith('/help'):
                 self.sender.sendMessage("Verfügbare Kommandos:\n\n"
                                         "/help diese Nachricht anzeigen\n"
@@ -93,21 +168,36 @@ class ChatUser(telepot.helper.ChatHandler):
             if self.verbose:
                 pprint(msg)
             location = msg.get('location')
-            self.sender.sendMessage('Berechen Abfahrten von:\nLon:\t {}\nLat:\t {}'.format(location.get('longitude'), location.get('latitude')))
+            self.getDepartures(location)
+
         else :
             self.sender.sendMessage(
                 'Dein "{}" ist im Nirwana gelandet ...'.format(content_type))
 
 
+    def getDepartures(self, location) :
+        global verbose
+        if verbose:
+            print("Calculating departures")
+            self.sender.sendMessage('Suche Haltestellen in der Naehe von:\nLon:\t {}\nLat:\t {}'.format(location.get('longitude'), location.get('latitude')))
+        station_list = get_stations_close_to(location)
+        if verbose:
+            self.sender.sendMessage('Abfahrten fuer Haltestellen abfragen: {}'.format(
+                str(list(map(getNameFromLocation, station_list))).replace("[","").replace("]","").replace("'","")
+            ))
+        for station in station_list :
+            departures = get_Departures_From_Location(station_list[0].get("id"))
+            foo = '*' + getNameFromLocation(station) +'* \n\n' + parseDepartures(departures)+ '\n\n\n'
+            self.sender.sendMessage(foo , parse_mode='Markdown')
+
 authorized_users = None
 bot = None
-scheduler = BackgroundScheduler()
 settings = easydict()
 
 
 
 def main():
-    global bot, authorized_users, alerting_on, verbose
+    global bot, authorized_users, alerting_on, mvg_auth_key, verbose, home_location
     config_filename = 'config.json'
     try:
         with open(config_filename, 'r') as config_file:
@@ -128,8 +218,14 @@ def main():
         print('Error: config file doesn’t contain an `authorized_users` list')
         return
 
+    mvg_auth_key = config.get('mvg_auth_key')
+    if not mvg_auth_key:
+        print('Error: config file doesn’t contain an `mvg_auth_key`')
+        return
+
     timeout_secs = config.get('timeout_secs',10*60)
     verbose = config.get('verbose')
+    home_location = config.get('home_location')
     # image_folder = config.get('image_folder', '/home/ftp-upload')
     alerting_on = True
     bot = telepot.DelegatorBot(telegram_bot_token, [
