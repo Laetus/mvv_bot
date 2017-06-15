c9b9ace48f70f8d02cb383825bd3403c5c447d65
Philipp Fröhlich <philipp-froehlich@hotmail.com>
2017-06-15 16:29:50 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
+"""Telegram Bot to access the API from the Munich Public Transport Network"""
 # -*- coding: utf-8 -*-
-
-import telepot
-import shelve
+import os
 import json
 import time
-import os
-import sys
-import subprocess
 import http.client
-import json2html
-import datetime
-
-from json2html import *
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-from telepot.delegate import per_chat_id_in, create_open, pave_event_space, include_callback_query_chat_id
 from pprint import pprint
+
+import telepot
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.delegate import per_chat_id_in, per_chat_id, create_open
+from telepot.delegate import pave_event_space, include_callback_query_chat_id
 
 APPNAME = "mvv_bot"
 APPVERSION = "1.0.0"
+MVG_AUTH_KEY = None
+AUTHORIZED_USERS = None
+BOT = None
+TELEGRAM_BOT_TOKEN = None
+VERBOSE = False
+HOME_LOCATION = None
 
+def get_mvg_auth_key():
+    """ returns the MVG api key"""
+    global MVG_AUTH_KEY
+    return MVG_AUTH_KEY
 
-def get_mvg_auth_key() :
-    global mvg_auth_key
-    return mvg_auth_key
-
-def get_stations_close_to(location) :
-    global verbose
+def get_stations_close_to(location):
+    """returns stations close to a given location"""
+    global VERBOSE
     latitude = location.get('latitude')
     longitude = location.get('longitude')
 
@@ -37,23 +39,24 @@ def get_stations_close_to(location) :
         'cache-control': "no-cache",
         }
 
-    conn.request("GET", '/fahrinfo/api/location/nearby?latitude={}&longitude={}'.format(latitude,longitude), headers=headers)
+    conn.request("GET", '/fahrinfo/api/location/nearby?latitude={}&longitude={}'
+                 .format(latitude, longitude), headers=headers)
 
     res = conn.getresponse()
     result = json.loads(res.read().decode("utf-8"))
 
     # order by distance desc
-    try : 
+    try:
         locations = result.get('locations')
         if len(locations) > 3:
             return locations[0:3]
-        else :
+        else:
             return locations
-    except ValueError as e:
+    except ValueError:
         return ""
 
-def getNameFromStation(x) : 
-
+def get_name_from_station(station):
+    """returns a string with the name, distance and products in"""
     products = {
         's' : "S", #S-Bahn
         'u' : "U", #U-Bahn
@@ -63,20 +66,19 @@ def getNameFromStation(x) :
 
     res = ''
 
-    for product in x.get("products"): 
+    for product in station.get("products"):
         if product in products:
             res = res + products.get(product)
-    
     try:
-        return x.get("name") + ' {} ({}m)'.format(res ,x.get("distance"))
+        return station.get("name") + ' {} ({}m)'.format(res, station.get("distance"))
     except ValueError:
         return ""
 
-def getLinesFromStation(x):
+def get_lines_from_station(station):
+    """ returns a string with all lines at one station"""
+    res = ''
 
-    res =  ''
-
-    linesMap = {
+    lines_shortcut_map = {
         "tram" : "T",
         "nachttram" : "NT",
         "sbahn" : "S",
@@ -88,25 +90,24 @@ def getLinesFromStation(x):
 
     delimiter = ", "
 
-    for lines in x.get("lines"):
-        if lines in linesMap:
-            for l in x.get("lines").get(lines) : 
-                res = res + linesMap.get(lines) + l + delimiter
-    
+    for lines in station.get("lines"):
+        if lines in lines_shortcut_map:
+            for line in station.get("lines").get(lines):
+                res = res + lines_shortcut_map.get(lines) + line + delimiter
     return res[0:len(res)-len(delimiter)]
 
-def projectDeparture(departure):
+def project_departure(departure):
+    """reduces the fields in departure object"""
     departure.pop("departureId", None)
     departure.pop("lineBackgroundColor", None)
     if not departure.get("sev"):
         departure.pop("sev", None)
-    
     return departure
 
 
-def get_Departures_From_Station(station_id) :
-    global verbose
-     
+def get_departures_from_station(station_id):
+    """ returns the next 10 departures at a given station"""
+    global VERBOSE
     conn = http.client.HTTPSConnection("www.mvg.de")
 
     headers = {
@@ -119,7 +120,7 @@ def get_Departures_From_Station(station_id) :
     res = conn.getresponse()
     result = json.loads(res.read().decode("utf-8"))
 
-    if verbose:
+    if VERBOSE:
         print(result)
 
     departures = result.get("departures")
@@ -127,51 +128,51 @@ def get_Departures_From_Station(station_id) :
     if len(departures) > 10:
         departures = departures[0:10]
 
-    return list(map(projectDeparture, departures))
+    return list(map(project_departure, departures))
 
 
-def parseDepartures(departures) :
-    result = "**Abfahrt**  **Linie**  **Ziel**\n" 
-    for departure in departures :
-        depTime = time.strftime("%H:%M" , time.localtime(departure.get("departureTime")/1e3))
-        result += '{}  {}   {}\n'.format(depTime, departure.get("product").capitalize()+departure.get("label") ,departure.get("destination"))
+def parse_departures(departures):
+    """returns departures in a table-like  string"""
+    result = "**Abfahrt**  **Linie**  **Ziel**\n"
+    for departure in departures:
+        dep_time = time.strftime("%H:%M", time.localtime(departure.get("departureTime")/1e3))
+        result += '{}  {}   {}\n'.format(
+            dep_time, departure.get("product").capitalize()+
+            departure.get("label"), departure.get("destination"))
 
     return result
-
-class easydict(dict):
+"""
+class Easydict(dict):
+    
     def __missing__(self, key):
-        self[key] = easydict()
+        self[key] = Easydict()
         return self[key]
-
+"""
 class ChatUser(telepot.helper.ChatHandler):
-
-    IdleMessages = ['tüdelü …', '*gähn*', 'Mir ist langweilig.', 'Chill dein Life! Alles cool hier.',
-                    'Nix los hier …',  'Hallo-o!!!', 'Alles cool, Digga.', 
+    """ models a chat user """
+    IdleMessages = ['tüdelü …', 'Mir ist langweilig.', 'Chill dein Life! Alles cool hier.',
+                    'Nix los hier …', 'Hallo-o!!!', 'Alles cool, Digga.',
                     'Mach du dein Ding. Ich mach hier meins.', 'Alles voll secure in da house.']
 
     def __init__(self, *args, **kwargs):
-        global verbose
+        global VERBOSE
         super(ChatUser, self).__init__(*args, **kwargs)
         self.timeout_secs = kwargs.get('timeout')
-        self.verbose = verbose
+        self.verbose = VERBOSE
 
     def open(self, initial_msg, seed):
-        content_type, chat_type, chat_id = telepot.glance(initial_msg)
-      
-    def on_idle(self, event):
-        global alerting_on
-        if alerting_on:
-            ridx = random.randint(0, len(ChatUser.IdleMessages) - 1)
-            self.sender.sendMessage(
-                ChatUser.IdleMessages[ridx], parse_mode='Markdown')
+        """ handle open event"""
+        print(str(seed))
+        telepot.glance(initial_msg)
 
     def on_close(self, msg):
+        """ handle close event """
         if self.verbose:
             print('on_close() called. {}'.format(msg))
         return True
 
     def send_main_menu(self):
-        global alerting_on
+        """ send main menu to user """
         kbd = [
             InlineKeyboardButton(text='Abfahrten', callback_data='dep')
         ]
@@ -179,7 +180,8 @@ class ChatUser(telepot.helper.ChatHandler):
         self.sender.sendMessage('Wähle eine Aktion:', reply_markup=keyboard)
 
     def on_chat_message(self, msg):
-        global settings, alerting_on, home_location
+        """ processes a chat message"""
+        global HOME_LOCATION
         content_type, chat_type, chat_id = telepot.glance(msg)
         if content_type == 'text':
             if self.verbose:
@@ -194,7 +196,7 @@ class ChatUser(telepot.helper.ChatHandler):
                                         parse_mode='Markdown')
                 self.send_main_menu()
             elif msg_text.startswith('/dep'):
-                self.getDepartures(home_location)
+                self.get_departures(HOME_LOCATION)
             elif msg_text.startswith('/help'):
                 self.sender.sendMessage("Verfügbare Kommandos:\n\n"
                                         "/help diese Nachricht anzeigen\n"
@@ -212,81 +214,82 @@ class ChatUser(telepot.helper.ChatHandler):
             if self.verbose:
                 pprint(msg)
             location = msg.get('location')
-            self.getDepartures(location)
+            self.get_departures(location)
 
-        else :
+        else:
             self.sender.sendMessage(
                 'Dein "{}" ist im Nirwana gelandet ...'.format(content_type))
 
 
-    def getDepartures(self, location) :
-        global verbose
-        if verbose:
+    def get_departures(self, location):
+        """sends the departures close to a given location to the user"""
+        global VERBOSE
+        if VERBOSE:
             print("Calculating departures")
-            self.sender.sendMessage('Suche Haltestellen in der Nähe von:\nLon:\t {}\nLat:\t {}'.format(location.get('longitude'), location.get('latitude')))
+            self.sender.sendMessage(
+                'Suche Haltestellen in der Nähe von:\nLon:\t {}\nLat:\t {}'
+                .format(location.get('longitude'), location.get('latitude')))
         station_list = get_stations_close_to(location)
-        if verbose:
+        if VERBOSE:
             self.sender.sendMessage('Abfahrten fuer Haltestellen abfragen: {}'.format(
-                str(list(map(getNameFromStation, station_list))).replace("[","").replace("]","").replace("'","")
+                str(list(map(get_name_from_station, station_list)))
+                .replace("[", "")
+                .replace("]", "")
+                .replace("'", "")
             ))
-        for station in station_list :
-            departures = get_Departures_From_Station(station_list[0].get("id"))
-            foo = '*' + getNameFromStation(station) +'*\n' +getLinesFromStation(station) + '\n\n' + parseDepartures(departures)+ '\n\n\n'
-            self.sender.sendMessage(foo , parse_mode='Markdown')
-
-authorized_users = None
-bot = None
-settings = easydict()
-
-
+        for station in station_list:
+            departures = get_departures_from_station(station_list[0].get("id"))
+            message = '*' + get_name_from_station(station) + '*\n' + get_lines_from_station(station) + '\n\n' + parse_departures(departures)+ '\n\n\n'
+            self.sender.sendMessage(message, parse_mode='Markdown')
 
 def main():
-    global bot, authorized_users, alerting_on, mvg_auth_key, verbose, home_location
-    config_filename = 'config/config.json'
+    """ runs the bot """
+    global BOT, AUTHORIZED_USERS, MVG_AUTH_KEY, VERBOSE, HOME_LOCATION, TELEGRAM_BOT_TOKEN
     try:
+        config_filename = "config/config.json"
         with open(config_filename, 'r') as config_file:
             config = json.load(config_file)
     except FileNotFoundError:
         print('Error: config file "{}" not found:'.format(config_filename))
         return
-    except ValueError as e:
-        print('Error: invalid config fie "{}": {}'.format(config_filename, e))
+    except ValueError as error:
+        print('Error: invalid config fie "{}": {}'.format(config_filename, error))
         return
-    telegram_bot_token = config.get('telegram_bot_token')
-    if not telegram_bot_token:
+
+    TELEGRAM_BOT_TOKEN = str(os.environ['TELEGRAM_BOT_TOKEN'])
+    if not TELEGRAM_BOT_TOKEN:
         print("Error: config file does not contain a 'telegram_bot_token'")
         return
 
-    authorized_users = config.get('authorized_users')
-    if type(authorized_users) is not list or len(authorized_users) <= 0:
+    AUTHORIZED_USERS = config.get('authorized_users')
+    if not isinstance(AUTHORIZED_USERS, list) or len(AUTHORIZED_USERS) <= 0:
         print('Error: config file doesn’t contain an `authorized_users` list')
         return
 
-    mvg_auth_key = config.get('mvg_auth_key')
-    if not mvg_auth_key:
+    MVG_AUTH_KEY = str(os.environ['MVG_AUTH_KEY'])
+    if not MVG_AUTH_KEY:
         print('Error: config file doesn’t contain an `mvg_auth_key`')
         return
 
-    timeout_secs = config.get('timeout_secs',10*60)
-    verbose = config.get('verbose')
-    home_location = config.get('home_location')
+    timeout_secs = config.get('timeout_secs', 10*60)
+    VERBOSE = str(os.environ['VERBOSE']) == 'true'
+    HOME_LOCATION = config.get('home_location')
     # image_folder = config.get('image_folder', '/home/ftp-upload')
-    alerting_on = True
-    bot = telepot.DelegatorBot(telegram_bot_token, [
-        include_callback_query_chat_id(pave_event_space())(per_chat_id_in(authorized_users, types='private'),
-                                                           create_open,
-                                                           ChatUser,
-                                                           timeout=timeout_secs)])
 
-    if verbose:
+    BOT = telepot.DelegatorBot(TELEGRAM_BOT_TOKEN, [
+        include_callback_query_chat_id(pave_event_space())(
+            per_chat_id(types='private'),
+            create_open,
+            ChatUser,
+            timeout=timeout_secs)])
+
+    if VERBOSE:
         print('Monitoring ...')
-   
     try:
-        bot.message_loop(run_forever='Bot listening ...')
+        BOT.message_loop(run_forever='Bot listening ...')
     except KeyboardInterrupt:
         pass
-    if verbose:
+    if VERBOSE:
         print('Exiting ...')
-   
 if __name__ == '__main__':
     main()
