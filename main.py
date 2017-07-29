95e01d6a41961fbb2973802b9bd551f4b94eed11
Philipp Fröhlich <philipp-froehlich@hotmail.com>
2017-07-29 13:05:10 +0200

    added text in /help message

--- main.py
+++ main.py
@@ -23,10 +23,12 @@ VERBOSE = False
 HOME_LOCATION = None
 USERS = None
 
+
 def get_mvg_auth_key():
     """ returns the MVG api key"""
     global MVG_AUTH_KEY
     return MVG_AUTH_KEY
+
 
 def get_stations_close_to(location):
     """returns stations close to a given location"""
@@ -39,7 +41,7 @@ def get_stations_close_to(location):
     headers = {
         'x-mvg-authorization-key': get_mvg_auth_key(),
         'cache-control': "no-cache",
-        }
+    }
 
     conn.request("GET", '/fahrinfo/api/location/nearby?latitude={}&longitude={}'
                  .format(latitude, longitude), headers=headers)
@@ -57,13 +59,14 @@ def get_stations_close_to(location):
     except ValueError:
         return ""
 
+
 def get_name_from_station(station):
     """returns a string with the name, distance and products in"""
     products = {
-        's' : "S", #S-Bahn
-        'u' : "U", #U-Bahn
-        't' : "T", #Tram
-        'b' : ""
+        's': "S",  # S-Bahn
+        'u': "U",  # U-Bahn
+        't': "T",  # Tram
+        'b': ""
     }
 
     res = ''
@@ -76,18 +79,19 @@ def get_name_from_station(station):
     except ValueError:
         return ""
 
+
 def get_lines_from_station(station):
     """ returns a string with all lines at one station"""
     res = ''
 
     lines_shortcut_map = {
-        "tram" : "T",
-        "nachttram" : "Nt",
-        "sbahn" : "S",
-        "ubahn" : "U",
-        "bus" : "",
-        "nachtbus" : "Nb",
-        "otherlines" : "X"
+        "tram": "T",
+        "nachttram": "Nt",
+        "sbahn": "S",
+        "ubahn": "U",
+        "bus": "",
+        "nachtbus": "Nb",
+        "otherlines": "X"
     }
 
     delimiter = ", "
@@ -96,7 +100,8 @@ def get_lines_from_station(station):
         if lines in lines_shortcut_map:
             for line in station.get("lines").get(lines):
                 res = res + lines_shortcut_map.get(lines) + line + delimiter
-    return res[0:len(res)-len(delimiter)]
+    return res[0:len(res) - len(delimiter)]
+
 
 def project_departure(departure):
     """reduces the fields in departure object"""
@@ -115,13 +120,14 @@ def get_departures_from_station(station_id):
     headers = {
         'x-mvg-authorization-key': get_mvg_auth_key(),
         'cache-control': "no-cache",
-        }
+    }
 
-    conn.request("GET", '/fahrinfo/api/departure/{}'.format(station_id), headers=headers)
+    conn.request(
+        "GET", '/fahrinfo/api/departure/{}'.format(station_id), headers=headers)
 
     res = conn.getresponse()
     result = json.loads(res.read().decode("utf-8"))
-    
+
     departures = result.get("departures")
 
     if len(departures) > 10:
@@ -134,12 +140,14 @@ def parse_departures(departures):
     """returns departures in a table-like  string"""
     result = "**Abfahrt**  **Linie**  **Ziel**\n"
     for departure in departures:
-        dep_time = time.strftime("%H:%M", time.localtime(departure.get("departureTime")/1e3))
+        dep_time = time.strftime("%H:%M", time.localtime(
+            departure.get("departureTime") / 1e3))
         result += '{}  {}   {}\n'.format(
-            dep_time, departure.get("product").capitalize()+
+            dep_time, departure.get("product").capitalize() +
             departure.get("label"), departure.get("destination"))
 
     return result
+
 
 def get_user_from_db(message):
     content_type, chat_type, chat_id = telepot.glance(message)
@@ -164,7 +172,7 @@ def get_user_from_db(message):
 
 class ChatUser(telepot.helper.ChatHandler):
     """ models a chat user """
-  
+
     def __init__(self, *args, **kwargs):
         global VERBOSE
         super(ChatUser, self).__init__(*args, **kwargs)
@@ -216,13 +224,16 @@ class ChatUser(telepot.helper.ChatHandler):
             elif msg_text.startswith('/dep'):
                 self.get_departures(user['home'])
             elif msg_text.startswith('/sethome'):
-                self.sender.sendMessage('If the next message is a location it is set as your new home.')
-                user['state'] = {'setHome': True, 'msg_number': int(user['msg_count'])}
+                self.sender.sendMessage(
+                    'If the next message is a location it is set as your new home.')
+                user['state'] = {'setHome': True,
+                                 'msg_number': int(user['msg_count'])}
             elif msg_text.startswith('/help'):
                 self.sender.sendMessage("Verfügbare Kommandos:\n\n"
                                         "/help diese Nachricht anzeigen\n"
                                         "/start den Bot (neu)starten\n"
-                                        "/dep Abfahrt von Location neu berechnen\n",
+                                        "/dep Abfahrt von deiner Wohnort aus neu berechnen\n"
+                                        "/sethome Wohnort neu setzen",
                                         parse_mode='Markdown')
             elif msg_text.startswith('/'):
                 self.sender.sendMessage(
@@ -246,9 +257,8 @@ class ChatUser(telepot.helper.ChatHandler):
         else:
             self.sender.sendMessage(
                 'Dein "{}" ist im Nirwana gelandet ...'.format(content_type))
-                
-        USERS.replace_one({'_id' : user.get('_id')}, user)
 
+        USERS.replace_one({'_id': user.get('_id')}, user)
 
     def get_departures(self, location):
         """sends the departures close to a given location to the user"""
@@ -268,8 +278,10 @@ class ChatUser(telepot.helper.ChatHandler):
             ))
         for station in station_list:
             departures = get_departures_from_station(station.get("id"))
-            message = '*' + get_name_from_station(station) + '*\n' + get_lines_from_station(station) + '\n\n' + parse_departures(departures)+ '\n\n\n'
+            message = '*' + get_name_from_station(station) + '*\n' + get_lines_from_station(
+                station) + '\n\n' + parse_departures(departures) + '\n\n\n'
             self.sender.sendMessage(message, parse_mode='Markdown')
+
 
 def main():
     """ runs the bot """
@@ -304,7 +316,7 @@ def main():
     database = client['mvv-bot']
     USERS = database['users']
 
-    timeout_secs = config.get('timeout_secs', 10*60)
+    timeout_secs = config.get('timeout_secs', 10 * 60)
     VERBOSE = str(os.environ['VERBOSE']) == 'true'
     HOME_LOCATION = config.get('home_location')
     # image_folder = config.get('image_folder', '/home/ftp-upload')
@@ -324,5 +336,7 @@ def main():
         pass
     if VERBOSE:
         print('Exiting ...')
+
+
 if __name__ == '__main__':
     main()
